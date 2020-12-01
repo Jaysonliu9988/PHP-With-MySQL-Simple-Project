6de96560ae2b49439d6990aa49f519b6185ac9b2
Jaysonliu <leobest9688@gmail.com>
2020-12-01 23:20:32 +0800

    create the index file

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+
+<html lang="en">
+    <head>
+    <title>PHP With MySQL Simple Project</title>
+    <meta charset="utf-8">
+    </head>
+
+    <body>
+        <header>
+            <h1>DBAL</h1>
+        </header>
+
+
+
+    </body>
+</html>
